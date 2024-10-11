c76180fa54762cb354d6719de70ba45b70a35ee1
TomasPytel <pytel5@stud.uniza.sk>
2024-10-11 13:24:39 +0200

    Navbar 1st view

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './Styles/App.css';
 import Navbar from './Components/Navbar';
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,35 @@
 import '../Styles/Navbar.css'
+import logoPic from '../Assets/Ehop.svg'
+import saleIcon from '../Assets/sale-icon.svg'
+import shoppingCartIcon from '../Assets/shopping-trolley-icon.svg'
+import userIcon from '../Assets/user-black-icon.svg'
 
 const Navbar = () => {
     return ( 
-        <div className="navbar container-fluid">
+        <div className="container-fluid navbar">
+            <a href='/' className='col-2 logo-container'>
+                <img className='logo' src={logoPic}></img>
+            </a>
+            <div className='searchbar-container col-5'>
+                <form>
+                    <input type='text' placeholder='Hľadať'></input>
+                    <button className='search'></button>
+                </form>
+            </div>
+            <div className='col-5 userlinks-container'>
+                <div className='userlinks'>
+                    <a href='/'>
+                        <img className='icon' alt='Zlavnene produkty' src={saleIcon}/>
+                    </a>
+                    <a href='/'>
+                        <img className='icon' alt='Nakupný košík' src={shoppingCartIcon}/>
+                    </a>
+                    <a href='/'>
+                        <img className='icon' alt='Užívateľský profil' src={userIcon}/>
+                </a>
+                </div>
+                <a className='registrationLink' href='/'>Registrovať sa</a>
+            </div>
         </div>
      );
 }
